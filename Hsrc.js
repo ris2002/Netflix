7973e2d041a1365df9f6e90200a931dbead5ef25
ris2002 <72793209+ris2002@users.noreply.github.com>
2022-10-12 07:01:22 +0530

    Create Hsrc.js

---
+++ Hsrc.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Bertv from './Bertv';
+
+import "./Hsrc.css";
+import Nav from './Nav';
+const Hsrc=()=>{
+    return(
+        <div className='homeScreen'>
+            <Nav/>
+           
+
+
+            {/*Nav*/}
+            <Bertv/>
+            
+
+            {/*Banner*/}
+
+            {/*Row*/}
+
+        </div>
+    )
+}
+export default Hsrc;
