20380bd526c991b74fa8ceeb3cc53b9415ed9675
ris2002 <72793209+ris2002@users.noreply.github.com>
2022-10-12 07:02:32 +0530

    Create Request.js

---
+++ Request.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-template-curly-in-string */
+//acc7209cb745b158aed71a3a6f719a49
+const API_KEY="acc7209cb745b158aed71a3a6f719a49";
+const requests ={
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+     fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`, 
+     fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+     fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+      fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    
+}
+export default requests;
